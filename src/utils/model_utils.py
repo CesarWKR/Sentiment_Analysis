a024be6de8027376628e010dfeafc33cb19d091e
César González <159592655+CesarWKR@users.noreply.github.com>
2025-06-08 19:39:33 -0500

    Add data distribution function that allows visualize the records per class

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import glob
+import matplotlib.pyplot as plt
 
 def get_latest_model_path(models_dir="."):
     """Get the latest fine-tuned model directory."""
@@ -23,3 +24,31 @@ def update_latest_model(models_dir=".", target_dir="bert_sentiment_model_inferen
         print(f"✅ Model copied to inference directory:  {target_dir}")
     else:
         print("⚠️ No model found to update.")
+
+
+def analyze_data_distribution(df, label_map={0: "Negative", 1: "Neutral", 2: "Positive"}):  # Function to analyze data distribution
+    """Analyzes data distribution by class and generates visual statistics with label names."""
+
+    # Map numeric labels to their names
+    df_mapped = df.copy()
+    df_mapped["label"] = df_mapped["label"].map(label_map)
+
+    label_counts = df_mapped["label"].value_counts()  # Count the number of records per class
+    label_order = ["Negative", "Neutral", "Positive"]  # Define desired order
+    label_counts = label_counts.reindex(label_order, fill_value=0)  # Reindex to match desired order, filling with 0 if a class is missing
+    print("🔢 Count of records per class:")
+    print(label_counts)
+
+    # Calculate the percentage of each class
+    percentages = label_counts / len(df) * 100
+    print("\n📊 Percentage of each class:")
+    print(percentages)
+
+    #   Plot the distribution of labels
+    plt.figure(figsize=(8, 5))
+    label_counts.plot(kind="bar", color=["red", "blue", "green"])
+    plt.xlabel("Sentiment")
+    plt.ylabel("Count")
+    plt.title("Distribution of labels in the dataset")
+    plt.xticks(rotation=0)
+    plt.show()
