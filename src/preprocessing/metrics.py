ba32962bcd1b6646762c800dec97821d94e201f7
César González <159592655+CesarWKR@users.noreply.github.com>
2025-06-08 19:40:29 -0500

    Define global varibles to get count metrics across modules

---
+++ src/preprocessing/metrics.py
@@ -0,0 +1,8 @@
+
+"""
+# This module defines shared metrics for text preprocessing across different modules.
+# It includes counters for invalid and empty texts, which can be used to track the quality of the data being processed.
+"""
+# Shared metrics across modules
+invalid_text_count = 0
+empty_text_count = 0
